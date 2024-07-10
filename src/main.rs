ef69180dc7dbc33c979334637988d8500b8e5a64
DavidLee18 <djwodus@gmail.com>
2024-07-10 15:40:50 +0900

    assign port & implement stop_streaming

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::{error::Error, process::Command};
 
-use axum::{extract::Path, http::StatusCode, routing::get, Router};
+use axum::{
+    extract::{Path, Query},
+    http::StatusCode,
+    routing::get,
+    Router,
+};
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -12,13 +18,26 @@ async fn main() -> std::io::Result<()> {
 
 async fn get_info(
     Path((proxy, wspw)): Path<(String, String)>,
+    port: Option<Query<Port>>,
 ) -> Result<String, (StatusCode, String)> {
+    let Query(Port { port }) = port.unwrap_or_default();
     let mut zrok_output = err(Command::new("zrok")
-        .args(["access", "private", proxy.as_str(), "--headless"])
+        .args([
+            "access",
+            "private",
+            proxy.as_str(),
+            "--headless",
+            "-b",
+            &format!("127.0.0.1:{}", port),
+        ])
         .spawn())?;
 
     let obs_output = err(Command::new("obs-cmd")
-        .args(["-w", &format!("obsws://localhost:9191/{}", wspw), "info"])
+        .args([
+            "-w",
+            &format!("obsws://localhost:{}/{}", port, wspw),
+            "info",
+        ])
         .output())?;
     err(zrok_output.kill())?;
     Ok(format!(
@@ -33,8 +52,51 @@ async fn get_info(
 }
 async fn stop_streaming(
     Path((proxy, wspw)): Path<(String, String)>,
+    port: Option<Query<Port>>,
 ) -> Result<String, (StatusCode, String)> {
-    todo!()
+    let Query(Port { port }) = port.unwrap_or_default();
+    let mut zrok_output = err(Command::new("zrok")
+        .args([
+            "access",
+            "private",
+            proxy.as_str(),
+            "--headless",
+            "-b",
+            &format!("127.0.0.1:{}", port),
+        ])
+        .spawn())?;
+
+    let obs_output = err(Command::new("obs-cmd")
+        .args([
+            "-w",
+            &format!("obsws://localhost:{}/{}", port, wspw),
+            "streaming",
+            "stop",
+        ])
+        .output())?;
+    err(zrok_output.kill())?;
+    Ok(format!(
+        "{:?}\n{:?}",
+        zrok_output,
+        String::from_utf8(if obs_output.stderr.is_empty() {
+            obs_output.stdout
+        } else {
+            obs_output.stderr
+        })
+    ))
+}
+
+#[derive(Deserialize)]
+struct Port {
+    port: String,
+}
+
+impl Default for Port {
+    fn default() -> Self {
+        Self {
+            port: String::from("9191"),
+        }
+    }
 }
 
 fn err<T, E>(r: Result<T, E>) -> Result<T, (StatusCode, String)>
